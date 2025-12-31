da951c686f9851e11f232912bd93e710f7bb4fbf
Rafael <rafaelmetallica@gmail.com>
2025-12-31 11:27:51 -0300

    Seed da base de dados, Equals hashCode

--- src/main/java/com/auladevsuperior/dscommerce/entities/OrderItemPK.java
+++ src/main/java/com/auladevsuperior/dscommerce/entities/OrderItemPK.java
@@ -2,6 +2,8 @@ package com.auladevsuperior.dscommerce.entities;
 
 import jakarta.persistence.*;
 
+import java.util.Objects;
+
 public class OrderItemPK {
 
     @ManyToOne
@@ -29,5 +31,20 @@ public class OrderItemPK {
 
     public void setProduct(Product product) {
         this.product = product;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        OrderItemPK that = (OrderItemPK) o;
+        return Objects.equals(order, that.order) && Objects.equals(product, that.product);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hashCode(order);
+        result = 31 * result + Objects.hashCode(product);
+        return result;
     }
 }
