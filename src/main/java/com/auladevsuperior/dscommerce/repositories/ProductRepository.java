9d3def0e6e91c9c07b6e9b797d48fca461b5c81c
Rafael <rafaelmetallica@gmail.com>
2026-03-02 21:29:01 -0300

    GET product by id

---
+++ src/main/java/com/auladevsuperior/dscommerce/repositories/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.auladevsuperior.dscommerce.repositories;
+
+import com.auladevsuperior.dscommerce.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository <Product, Long> {
+}
