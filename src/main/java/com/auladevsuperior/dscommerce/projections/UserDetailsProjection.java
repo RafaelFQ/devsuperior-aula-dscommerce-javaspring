d9a4eb969b38fcc9446f43eaa33f6ab672762f20
Rafael <rafaelmetallica@gmail.com>
2026-03-19 20:37:20 -0300

    UserDetailsService

---
+++ src/main/java/com/auladevsuperior/dscommerce/projections/UserDetailsProjection.java
@@ -0,0 +1,9 @@
+package com.auladevsuperior.dscommerce.projections;
+
+public interface UserDetailsProjection {
+
+    String getUsername();
+    String getPassword();
+    Long getRoleId();
+    String getAuthority();
+}
